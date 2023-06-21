7a2086ea8f15ccfe02939c02e3c3266cd8b3e05e
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-06-21 10:17:01 +0200

    feat: more domains (#45)
    * Add more proton related domains
    
    * refactor: better script generation
    
    ---------
    
    Co-authored-by: eoin-pledge <eoin@yourpledge.earth>

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,20 +1,23 @@
-import jsonFuture from 'json-future'
-import got from 'got'
+import { writeFile } from 'node:fs/promises'
 
 /* List of free email domains by HubSpot
    https://knowledge.hubspot.com/forms/what-domains-are-blocked-when-using-the-forms-email-domains-to-block-feature */
 const URL =
   'https://f.hubspotusercontent40.net/hubfs/2832391/Marketing/Lead-Capture/free-domains-2.csv'
 
+/** Additional domains not present in the URL */
+const DOMAINS = ['pm.me', 'proton.me', 'protonmail.ch']
+
 const trim = text => text.replace(/^\s+|\s+$/g, '')
 
-const save = async body => {
-  const result = body.split(/[,\n]/g).map(trim).filter(Boolean)
-  await jsonFuture.saveAsync('domains.json', result)
-}
+const sanetize = text => text.split(/[,\n]/g).map(trim).filter(Boolean)
 
 try {
-  await save(await got(URL).text())
+  const raw = await fetch(URL).then(res => res.text())
+  const domains = new Set(sanetize(raw))
+  for (const domain of DOMAINS) domains.add(domain)
+  const sorted = Array.from(domains).sort()
+  await writeFile('domains.json', JSON.stringify(sorted, null, 2))
 } catch (error) {
   console.error(error)
   process.exit(1)
